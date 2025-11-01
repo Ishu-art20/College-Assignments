c0b3fd06f386495f45d027aad3cb1378312326ff
Ishu <cishuu208206@gmail.com>
2025-11-01 21:41:18 +0530

    Add files via upload

---
+++ FOCP1-Assignment/Q12.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i, score[]={34,45,67,89,62,81};
+    int n,max,min;
+    n = sizeof(score)/sizeof(score[0]); 
+    min=score[0]; 
+    max=score[0];
+    for(i=0;i<n;i++)
+    {  
+       if(max<score[i])
+       max=score[i];
+       if(min>score[i])
+       min=score[i];
+    }
+    printf("Maximum score = %d Minimum scores are = %d",max,min);
+}
