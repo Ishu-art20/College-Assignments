4bfb80a6b11fb897f37ee11888abf972ee7f501c
Ishu <cishuu208206@gmail.com>
2025-11-01 15:51:37 +0530

    Implement Armstrong number checker in Q1.c

--- FOCP1-Assignment/Q1.c
+++ FOCP1-Assignment/Q1.c
@@ -1,1 +1,29 @@
-# Code-journey
+// Armstrong Number
+#include<stdio.h>
+#include<math.h>
+int main()
+{ 
+    int no,count=0,copy;
+    int rem,rev=0,arm=0;
+    printf("Enter number: ");
+    scanf("%d",&no);
+    copy= no;
+    count=0;
+    while(no>0)
+    {   count++;
+        no=no/10;    
+    }
+    no=copy;
+    while(no>0)
+    {
+        rem=no%10;
+        arm=arm+ceil(pow(rem,count));
+        no=no/10;
+    }
+    if(copy==arm)
+    printf("\nIt's an Armstrong Number");
+    else
+    printf("\nIt's not an Armstrong Number");
+    return 0;
+
+}
