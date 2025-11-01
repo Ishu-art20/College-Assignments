e1faec378d534263c610f3a998ebbd9dd0bbb4a4
Ishu <cishuu208206@gmail.com>
2025-11-01 16:30:25 +0530

    Add files via upload

---
+++ FOCP1-Assignment/Q5.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    printf("Enter Coordinates(x,y): ");
+    scanf("%d %d",&x,&y);
+    if(x==0 && y==0)
+      printf("Point lies at origin");
+    else if(x==0) 
+      printf("Point lies on Y-axis");
+    else if(y==0) 
+      printf("Point lies on X-axis");
+    else if(x>0 && y>0) 
+      printf("Point lies in FIRST Quadrant");
+    else if(x<0 && y>0) 
+      printf("Point lies in SECOND Quadrant");
+    else if(x<0 && y<0) 
+      printf("Point lies in THIRD Quadrant");
+    else 
+      printf("Point lies in FOURTH Quadrant");
+
+    return 0;
+}
