d0e0b81f37284d132fcf70c31a8c5481ade0d037
Ishu <cishuu208206@gmail.com>
2025-11-01 20:12:41 +0530

    Add files via upload

---
+++ FOCP1-Assignment/Q9.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,count=-1;
+    printf("Enter number of players : ");
+    scanf("%d",&n);
+    int score[n];
+    printf("Enter scores of %d players:",n);
+    for(i=0;i<n;i++)
+    {  
+        scanf("%d",&score[i]); 
+    } 
+    for(i=0;i<n;i++)
+    {   if (score[i]==99)
+       { count=i;  
+        break; }   
+    } 
+     if(count!=-1)
+     printf("First occurence of score 99 is at %dth position",count+=1);
+    else
+     printf("NO one got 99 score");
+    return 0;
+}
