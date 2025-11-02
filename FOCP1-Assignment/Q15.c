74413ae44409f36fe1cb1b59bcce49cfc2fc05fb
Ishu <cishuu208206@gmail.com>
2025-11-02 15:24:50 +0530

    Add files via upload

---
+++ FOCP1-Assignment/Q15.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) {
+    int arr[] = {1,2,3,4,5}; 
+    int last,i,n;      
+    n = sizeof(arr)/sizeof(arr[0]);
+    if (n>1) 
+    {
+        last=arr[n-1];            
+        for (i=n-1;i>0;--i)       
+            arr[i]=arr[i-1];
+            arr[0]=last;                   
+    }
+    printf("rotated array: ");
+    for (i=0;i<n;++i) 
+        printf("%d ",arr[i]);
+        printf("\n");
+}
