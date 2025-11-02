10235859a8899c650847773508579c85afc7e830
Ishu <cishuu208206@gmail.com>
2025-11-02 20:46:18 +0530

    Add files via upload

---
+++ FOCP1-Assignment/Q18.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int arr[]={1,7,3,2,7,1,2,3,2};
+    int n,i,f=0,freq[100]={0};
+    n=sizeof(arr)/sizeof(arr[0]);
+    for(i=0;i<n;i++)
+    {
+        freq[arr[i]]++;
+    }
+    printf("Duplicat element are: ");
+    for(i=0;i<100;i++)
+    {
+        if(freq[i]>1)
+        {    printf("%d ",i);
+             f=1;   }
+    }
+    if(!f)
+      printf("-1 (No Duplicates)");
+    return 0;
+}
